7abc0b0eae813f114fd5e8751eacbf6115fe1e20
Aragoorn <alirostami9080@gmail.com>
2026-08-15 16:32:34 +0400

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,12 +16,27 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-  giwaSepolia: {
-    url: process.env.GIWA_RPC_URL || "https://sepolia-rpc.giwa.io",
-    accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-    chainId: 91342,
+    giwaSepolia: {
+      url: process.env.GIWA_RPC_URL || "https://sepolia-rpc.giwa.io",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 91342,
+    },
   },
- },
+  etherscan: {
+    apiKey: {
+      giwaSepolia: "abc", // یا "no-api-key" اگر API Key لازم نداشت، خالی بگذار یا هر چیزی بگذار
+    },
+    customChains: [
+      {
+        network: "giwaSepolia",
+        chainId: 91342,
+        urls: {
+          apiURL: "https://sepolia-explorer.giwa.io/api",
+          browserURL: "https://sepolia-explorer.giwa.io",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
