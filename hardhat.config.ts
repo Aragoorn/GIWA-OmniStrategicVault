645f9e6be786143aa917d04f0455c82f9789ceff
Aragoorn <alirostami9080@gmail.com>
2026-08-15 15:52:35 +0400

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.34",
-  networks: {
-    giwaSepolia: {
-      url: "https://sepolia.giwa.io", // (یا RPC رسمی شبکه تستنت گیوا)
-      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
   },
+  networks: {
+  giwaSepolia: {
+    url: process.env.GIWA_RPC_URL || "https://sepolia-rpc.giwa.io",
+    accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    chainId: 91342,
+  },
+ },
 };
 
 export default config;
